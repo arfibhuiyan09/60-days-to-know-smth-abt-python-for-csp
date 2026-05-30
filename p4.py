af3e35eb29b32b31f391fab9dbd9f8fc2ae52be0
arfibhuiyan09 <144164033+arfibhuiyan09@users.noreply.github.com>
2026-05-30 15:01:45 -0400

    p3.py but revamped to use for loops and lists
    shoutout to gemini for teaching me what enumerating is

---
+++ p4.py
@@ -0,0 +1,28 @@
+print("math quiz:")
+choice = input("do you want to play (Y/N)").lower()
+count = 0
+questions = ["What is 2-3", 
+             "What is (2)^2 - 4", 
+             "What is (10)/(2)^2 + sqrt(64)/2", 
+             "simplify: 2^(2x) - 4^x - 32", 
+             "simpify (1/sin(x)) * cscx * tanx in terms only with cosine in the first quadrant of the Unit Circle"]
+
+Q5approved_answers = ["(1/(cos(x)(sqrt(1-cos^2(x)))", 
+                      "1/(cos(x)*sqrt(1-cos^2(x)))", 
+                      "1/(cos(x)(sqrt(1-cos^2(x))))"]
+
+answers = [["-1"], ["0"], ["6.5"], ["-32"], Q5approved_answers]
+if choice == "y":
+    for i, question in enumerate(questions):
+        print(question)
+        answer = input("answer the question:").lower()
+        if answer in answers[i]:
+            print("good job")
+            count += 1
+        else:
+            print("no")
+    print(f"you have gotten {count}/5 correct, which is a {(count/5)*100}%!")
+elif choice == "n":
+    print("oka")
+else:
+    print("invalid option")
