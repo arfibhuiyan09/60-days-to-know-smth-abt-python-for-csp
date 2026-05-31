b274c5105c9e0d15452dcc12a52382058df739bf
arfibhuiyan09 <144164033+arfibhuiyan09@users.noreply.github.com>
2026-05-31 14:34:56 -0400

    update 2 player battle sim health due it being too confusing
    i think thats all the major bugs fixed, theres a another bug but idk how to fix it so yeah\g

--- p8.py
+++ p8.py
@@ -16,12 +16,13 @@ def fighter_initialization():
 
 def battle(fighters):
     #default health
-    # also health 1 = player 2, health 2 = player 1. very counterintuative i know. 
-    health1 = 100
-    health2 = 100
+    fighter1_hp = 100
+    fighter2_hp = 100
+
     #show default health
-    print(f"{fighters['name1']} has {health1} HP,")
-    print(f"{fighters['name2']} has {health2} HP,")
+    print(f"{fighters['name1']} has {fighter1_hp} HP,")
+    print(f"{fighters['name2']} has {fighter2_hp} HP,")
+
     while True:
         #define damage1a, damage 1b, damage 2a, damage 2b
         damage1a = 0
@@ -31,70 +32,82 @@ def battle(fighters):
 
         #ask fighter 1 for input
         selection1 = input(f"it is {fighters['name1']}'s turn, which ability would you like to pick? ({fighters['attack1a']}, or {fighters['attack1b']})  ")
+
         if selection1 == fighters['attack1a']:
           #calculates damage by choosing a random number
           damage1a = random.randint(10,25)
           print(f"{fighters['name1']} has attacked {fighters['name2']} with {fighters['attack1a']} and did {damage1a} dmg!")
+
           #if player 1 attacks using attack1a
-          updated_health1a = health1-damage1a
+          fighter2_hp = fighter2_hp - damage1a
+
           #prints how much health player 2 has
-          print(f"{fighters['name2']} has {updated_health1a} HP")
-          health1=updated_health1a
+          print(f"{fighters['name2']} has {fighter2_hp} HP")
+
         elif selection1 == fighters['attack1b']:
             #calculates damage by choosing a random number
             damage1b = random.randint(1,40)
             print(f"{fighters['name1']} has attacked {fighters['name2']} with {fighters['attack1b']} and did {damage1b} dmg!")
+
             #if player 1 attacks using attack1b
-            updated_health1b = health1-damage1b
+            fighter2_hp = fighter2_hp - damage1b
+
             #prints how much health player 2 has
-            print(f"{fighters['name2']} has {updated_health1b} HP")
-            health1=updated_health1b
+            print(f"{fighters['name2']} has {fighter2_hp} HP")
+
         else:
             print(f"--> Error: '{selection1}' is an invalid prompt answer!")
 
-         #check if the health for both players have depleted fully
-        if health1 <= 0:
-            print(f"GGs, {fighters['name1']} wins! with {health2} HP remaining!")
+        #check if the health for both players have depleted fully
+        if fighter2_hp <= 0:
+            print(f"GGs, {fighters['name1']} wins! with {fighter1_hp} HP remaining!")
             break
         else:
             pass
-        if health2 <=0:
-            print(f"GGs, {fighters['name2']} wins! with {health2} HP remaining!")
+
+        if fighter1_hp <= 0:
+            print(f"GGs, {fighters['name2']} wins! with {fighter2_hp} HP remaining!")
             break
         else:
             pass
-        
+
         #ask fighter 2 for input
         selection2 = input(f"it is {fighters['name2']}'s turn, which ability would you like to pick? ({fighters['attack2a']}, or {fighters['attack2b']})   ")
+
         if selection2 == fighters['attack2a']:
           #calculates damage by choosing a random number
           damage2a = random.randint(10,25)
           print(f"{fighters['name2']} has attacked {fighters['name1']} with {fighters['attack2a']} and did {damage2a} dmg!")
+
           #if player 2 attacks using attack2a
-          updated_health2a = health2-damage2a
+          fighter1_hp = fighter1_hp - damage2a
+
           #prints how much health player 1 has
-          print(f"{fighters['name1']} has {updated_health2a} HP")
-          health2=updated_health2a
+          print(f"{fighters['name1']} has {fighter1_hp} HP")
+
         elif selection2 == fighters['attack2b']:
             #calculates damage by choosing a random number
             damage2b = random.randint(1,40)
             print(f"{fighters['name2']} has attacked {fighters['name1']} with {fighters['attack2b']} and did {damage2b} dmg!")
+
             #if player 2 attacks using attack2b
-            updated_health2b = health2-damage2b
+            fighter1_hp = fighter1_hp - damage2b
+
             #prints how much health player 1 has
-            print(f"{fighters['name1']} has {updated_health2b} HP")
-            health2=updated_health2b
+            print(f"{fighters['name1']} has {fighter1_hp} HP")
+
         else:
             print(f"--> Error: '{selection2}' is an invalid prompt answer!")
 
         #check if the health for both players have depleted fully
-        if health1 <= 0:
-            print(f"GGs, {fighters['name1']} wins! with {health2} HP remaining!")
+        if fighter2_hp <= 0:
+            print(f"GGs, {fighters['name1']} wins! with {fighter1_hp} HP remaining!")
             break
         else:
             pass
-        if health2 <=0:
-            print(f"GGs, {fighters['name2']} wins! with {health1} HP remaining!")
+
+        if fighter1_hp <= 0:
+            print(f"GGs, {fighters['name2']} wins! with {fighter2_hp} HP remaining!")
             break
         else:
             pass
